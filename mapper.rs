7d63a6fb029bfd8dec9a88e263f25c0ef97f24ea
Patrick Walton <pcwalton@mimiga.net>
2013-01-01 23:33:34 -0800

    Make mapper not use the GC

--- mapper.rs
+++ mapper.rs
@@ -12,38 +12,44 @@ pub struct Mapper {
     rom: &Rom,
 
     // FIXME: ~fn is awful here; we need the function region work.
-    // FIXME: We may want to trait-ify this. It's hard because of the region in Rom though. Perhaps
-    // we need region-parameterized &Traits.
-    loadb: ~fn(this: &Mapper, addr: u16) -> u8;
-    storeb: ~fn(this: &Mapper, addr: u16, val: u8);
-    loadw: ~fn(this: &Mapper, addr: u16) -> u16;
-    storew: ~fn(this: &Mapper, addr: u16, val: u16);
-    iface: @IMapper,    // FIXME: segfaults if I use ~IMapper, ugh
+    // FIXME: We should trait-ify this. It's hard because of the region in Rom though. I guess we
+    // need region-parameterized &Traits to fix this. Dependent on the function region work.
+    // NB: This exposed a nasty Rust bug! ~IMapper segfaults; see "segfault" branch.
+    loadb: ~fn(this: &mut Mapper, addr: u16) -> u8,
+    storeb: ~fn(this: &mut Mapper, addr: u16, val: u8),
+    loadw: ~fn(this: &mut Mapper, addr: u16) -> u16,
+    storew: ~fn(this: &mut Mapper, addr: u16, val: u16),
 }
 
-pub trait IMapper {
-    fn loadb(&mut self, rom: &Rom, addr: u16) -> u8;
-    fn storeb(&mut self, rom: &Rom, addr: u16, val: u8);
-    fn loadw(&mut self, rom: &Rom, addr: u16) -> u16;
-    fn storew(&mut self, rom: &Rom, addr: u16, val: u16);
+trait IMapper {
+    fn loadb(self, this: &mut Mapper, addr: u16) -> u8;
+    fn storeb(self, this: &mut Mapper, addr: u16, val: u8);
+    fn loadw(self, this: &mut Mapper, addr: u16) -> u16;
+    fn storew(self, this: &mut Mapper, addr: u16, val: u16);
 }
 
 pub impl Mapper {
     static fn new(rom: &a/Rom) -> Mapper/&a {
-        let iface = match rom.header.mapper() {
-            0 => @Nrom as @IMapper,
+        match rom.header.mapper() {
+            0 => Mapper {
+                rom: rom,
+
+                loadb:  |this, addr|      Nrom.loadb(this, addr),
+                storeb: |this, addr, val| Nrom.storeb(this, addr, val),
+                loadw:  |this, addr|      Nrom.loadw(this, addr),
+                storew: |this, addr, val| Nrom.storew(this, addr, val)
+            },
             _ => fail ~"unsupported mapper"
-        };
-        Mapper { rom: rom, iface: iface }
+        }
     }
 }
 
 // Forward onto the interface.
 pub impl Mapper : Mem {
-    fn loadb(&mut self, addr: u16) -> u8      { self.iface.loadb(self.rom, addr) }
-    fn storeb(&mut self, addr: u16, val: u8)  { self.iface.storeb(self.rom, addr, val) }
-    fn loadw(&mut self, addr: u16) -> u16     { self.iface.loadw(self.rom, addr) }
-    fn storew(&mut self, addr: u16, val: u16) { self.iface.storew(self.rom, addr, val) }
+    fn loadb(&mut self, addr: u16) -> u8      { (self.loadb)(self, addr) }
+    fn storeb(&mut self, addr: u16, val: u8)  { (self.storeb)(self, addr, val) }
+    fn loadw(&mut self, addr: u16) -> u16     { (self.loadw)(self, addr) }
+    fn storew(&mut self, addr: u16, val: u16) { (self.storew)(self, addr, val) }
 }
 
 //
@@ -57,23 +63,23 @@ pub struct Nrom;
 
 // TODO: Support 32K ROMs (NROM-256).
 pub impl Nrom : IMapper {
-    fn loadb(&mut self, rom: &Rom, addr: u16) -> u8 {
+    fn loadb(self, this: &mut Mapper, addr: u16) -> u8 {
         if addr <= 0x8000 {
             0   // FIXME
         } else {
-            rom.prg[addr & 0x3fff]
+            this.rom.prg[addr & 0x3fff]
         }
     }
-    fn storeb(&mut self, _: &Rom, _: u16, _: u8) {
+    fn storeb(self, _: &mut Mapper, _: u16, _: u8) {
         // TODO
     }
 
-    fn loadw(&mut self, rom: &Rom, addr: u16) -> u16 {
+    fn loadw(self, this: &mut Mapper, addr: u16) -> u16 {
         // FIXME: On x86 use unsafe code to do an unaligned read.
-        self.loadb(rom, addr) as u16 | (self.loadb(rom, addr + 1) as u16 << 8)
+        self.loadb(this, addr) as u16 | (self.loadb(this, addr + 1) as u16 << 8)
     }
 
-    fn storew(&mut self, _: &Rom, _: u16, _: u16) {
+    fn storew(self, _: &mut Mapper, _: u16, _: u16) {
         // TODO
     }
 }
