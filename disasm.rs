edb228fa45affeff7105dbdb647501151423d1c0
Patrick Walton <pcwalton@mimiga.net>
2013-12-31 23:02:20 -0800

    Stop using ~"" in the disassembler

--- disasm.rs
+++ disasm.rs
@@ -39,104 +39,104 @@ impl<M:Mem> Disassembler<M> {
     // TODO: When we get method macros some of this ugly duplication can go away.
 
     // Loads
-    fn lda(&mut self, am: ~str) -> ~str { ~"LDA " + am }
-    fn ldx(&mut self, am: ~str) -> ~str { ~"LDX " + am }
-    fn ldy(&mut self, am: ~str) -> ~str { ~"LDY " + am }
+    fn lda(&mut self, am: ~str) -> ~str { format!("LDA {}", am) }
+    fn ldx(&mut self, am: ~str) -> ~str { format!("LDX {}", am) }
+    fn ldy(&mut self, am: ~str) -> ~str { format!("LDY {}", am) }
 
     // Stores
-    fn sta(&mut self, am: ~str) -> ~str { ~"STA " + am }
-    fn stx(&mut self, am: ~str) -> ~str { ~"STX " + am }
-    fn sty(&mut self, am: ~str) -> ~str { ~"STY " + am }
+    fn sta(&mut self, am: ~str) -> ~str { format!("STA {}", am) }
+    fn stx(&mut self, am: ~str) -> ~str { format!("STX {}", am) }
+    fn sty(&mut self, am: ~str) -> ~str { format!("STY {}", am) }
 
     // Arithmetic
-    fn adc(&mut self, am: ~str) -> ~str { ~"ADC " + am }
-    fn sbc(&mut self, am: ~str) -> ~str { ~"SBC " + am }
+    fn adc(&mut self, am: ~str) -> ~str { format!("ADC {}", am) }
+    fn sbc(&mut self, am: ~str) -> ~str { format!("SBC {}", am) }
 
     // Comparisons
-    fn cmp(&mut self, am: ~str) -> ~str { ~"CMP " + am }
-    fn cpx(&mut self, am: ~str) -> ~str { ~"CPX " + am }
-    fn cpy(&mut self, am: ~str) -> ~str { ~"CPY " + am }
+    fn cmp(&mut self, am: ~str) -> ~str { format!("CMP {}", am) }
+    fn cpx(&mut self, am: ~str) -> ~str { format!("CPX {}", am) }
+    fn cpy(&mut self, am: ~str) -> ~str { format!("CPY {}", am) }
 
     // Bitwise operations
-    fn and(&mut self, am: ~str) -> ~str { ~"AND " + am }
-    fn ora(&mut self, am: ~str) -> ~str { ~"ORA " + am }
-    fn eor(&mut self, am: ~str) -> ~str { ~"EOR " + am }
-    fn bit(&mut self, am: ~str) -> ~str { ~"BIT " + am }
+    fn and(&mut self, am: ~str) -> ~str { format!("AND {}", am) }
+    fn ora(&mut self, am: ~str) -> ~str { format!("ORA {}", am) }
+    fn eor(&mut self, am: ~str) -> ~str { format!("EOR {}", am) }
+    fn bit(&mut self, am: ~str) -> ~str { format!("BIT {}", am) }
 
     // Shifts and rotates
-    fn rol(&mut self, am: ~str) -> ~str { ~"ROL " + am }
-    fn ror(&mut self, am: ~str) -> ~str { ~"ROR " + am }
-    fn asl(&mut self, am: ~str) -> ~str { ~"ASL " + am }
-    fn lsr(&mut self, am: ~str) -> ~str { ~"LSR " + am }
+    fn rol(&mut self, am: ~str) -> ~str { format!("ROL {}", am) }
+    fn ror(&mut self, am: ~str) -> ~str { format!("ROR {}", am) }
+    fn asl(&mut self, am: ~str) -> ~str { format!("ASL {}", am) }
+    fn lsr(&mut self, am: ~str) -> ~str { format!("LSR {}", am) }
 
     // Increments and decrements
-    fn inc(&mut self, am: ~str) -> ~str { ~"INC " + am }
-    fn dec(&mut self, am: ~str) -> ~str { ~"DEC " + am }
-    fn inx(&mut self) -> ~str           { ~"INX"       }
-    fn dex(&mut self) -> ~str           { ~"DEX"       }
-    fn iny(&mut self) -> ~str           { ~"INY"       }
-    fn dey(&mut self) -> ~str           { ~"DEY"       }
+    fn inc(&mut self, am: ~str) -> ~str { format!("INC {}", am) }
+    fn dec(&mut self, am: ~str) -> ~str { format!("DEC {}", am) }
+    fn inx(&mut self) -> ~str           { "INX".to_str()       }
+    fn dex(&mut self) -> ~str           { "DEX".to_str()       }
+    fn iny(&mut self) -> ~str           { "INY".to_str()       }
+    fn dey(&mut self) -> ~str           { "DEY".to_str()       }
 
     // Register moves
-    fn tax(&mut self) -> ~str           { ~"TAX"       }
-    fn tay(&mut self) -> ~str           { ~"TAY"       }
-    fn txa(&mut self) -> ~str           { ~"TXA"       }
-    fn tya(&mut self) -> ~str           { ~"TYA"       }
-    fn txs(&mut self) -> ~str           { ~"TXS"       }
-    fn tsx(&mut self) -> ~str           { ~"TSX"       }
+    fn tax(&mut self) -> ~str           { "TAX".to_str()       }
+    fn tay(&mut self) -> ~str           { "TAY".to_str()       }
+    fn txa(&mut self) -> ~str           { "TXA".to_str()       }
+    fn tya(&mut self) -> ~str           { "TYA".to_str()       }
+    fn txs(&mut self) -> ~str           { "TXS".to_str()       }
+    fn tsx(&mut self) -> ~str           { "TSX".to_str()       }
 
     // Flag operations
-    fn clc(&mut self) -> ~str           { ~"CLC"       }
-    fn sec(&mut self) -> ~str           { ~"SEC"       }
-    fn cli(&mut self) -> ~str           { ~"CLI"       }
-    fn sei(&mut self) -> ~str           { ~"SEI"       }
-    fn clv(&mut self) -> ~str           { ~"CLV"       }
-    fn cld(&mut self) -> ~str           { ~"CLD"       }
-    fn sed(&mut self) -> ~str           { ~"SED"       }
+    fn clc(&mut self) -> ~str           { "CLC".to_str()       }
+    fn sec(&mut self) -> ~str           { "SEC".to_str()       }
+    fn cli(&mut self) -> ~str           { "CLI".to_str()       }
+    fn sei(&mut self) -> ~str           { "SEI".to_str()       }
+    fn clv(&mut self) -> ~str           { "CLV".to_str()       }
+    fn cld(&mut self) -> ~str           { "CLD".to_str()       }
+    fn sed(&mut self) -> ~str           { "SED".to_str()       }
 
     // Branches
     // FIXME: Should disassemble the displacement!
-    fn bpl(&mut self) -> ~str           { ~"BPL xx"    }
-    fn bmi(&mut self) -> ~str           { ~"BMI xx"    }
-    fn bvc(&mut self) -> ~str           { ~"BVC xx"    }
-    fn bvs(&mut self) -> ~str           { ~"BVS xx"    }
-    fn bcc(&mut self) -> ~str           { ~"BCC xx"    }
-    fn bcs(&mut self) -> ~str           { ~"BCS xx"    }
-    fn bne(&mut self) -> ~str           { ~"BNE xx"    }
-    fn beq(&mut self) -> ~str           { ~"BEQ xx"    }
+    fn bpl(&mut self) -> ~str           { "BPL xx".to_str()    }
+    fn bmi(&mut self) -> ~str           { "BMI xx".to_str()    }
+    fn bvc(&mut self) -> ~str           { "BVC xx".to_str()    }
+    fn bvs(&mut self) -> ~str           { "BVS xx".to_str()    }
+    fn bcc(&mut self) -> ~str           { "BCC xx".to_str()    }
+    fn bcs(&mut self) -> ~str           { "BCS xx".to_str()    }
+    fn bne(&mut self) -> ~str           { "BNE xx".to_str()    }
+    fn beq(&mut self) -> ~str           { "BEQ xx".to_str()    }
 
     // Jumps
     // FIXME: Should disassemble the address!
-    fn jmp(&mut self) -> ~str           { ~"JMP xx"    }
-    fn jmpi(&mut self) -> ~str          { ~"JMP (xx)"  }
+    fn jmp(&mut self) -> ~str           { "JMP xx".to_str()    }
+    fn jmpi(&mut self) -> ~str          { "JMP (xx)".to_str()  }
 
     // Procedure calls
     // FIXME: Should disassemble the address!
-    fn jsr(&mut self) -> ~str           { ~"JSR xx"    }
-    fn rts(&mut self) -> ~str           { ~"RTS"       }
-    fn brk(&mut self) -> ~str           { ~"BRK"       }
-    fn rti(&mut self) -> ~str           { ~"RTI"       }
+    fn jsr(&mut self) -> ~str           { "JSR xx".to_str()    }
+    fn rts(&mut self) -> ~str           { "RTS".to_str()       }
+    fn brk(&mut self) -> ~str           { "BRK".to_str()       }
+    fn rti(&mut self) -> ~str           { "RTI".to_str()       }
 
     // Stack operations
-    fn pha(&mut self) -> ~str           { ~"PHA"       }
-    fn pla(&mut self) -> ~str           { ~"PLA"       }
-    fn php(&mut self) -> ~str           { ~"PHP"       }
-    fn plp(&mut self) -> ~str           { ~"PLP"       }
+    fn pha(&mut self) -> ~str           { "PHA".to_str()       }
+    fn pla(&mut self) -> ~str           { "PLA".to_str()       }
+    fn php(&mut self) -> ~str           { "PHP".to_str()       }
+    fn plp(&mut self) -> ~str           { "PLP".to_str()       }
 
     // No operation
-    fn nop(&mut self) -> ~str           { ~"NOP"       }
+    fn nop(&mut self) -> ~str           { "NOP".to_str()       }
 
     // Addressing modes
-    fn immediate(&mut self) -> ~str          { ~"#" + self.disb_bump_pc()          }
-    fn accumulator(&mut self) -> ~str        { ~""                                 }
-    fn zero_page(&mut self) -> ~str          { self.disb_bump_pc()                 }
-    fn zero_page_x(&mut self) -> ~str        { self.disb_bump_pc() + ",X"          }
-    fn zero_page_y(&mut self) -> ~str        { self.disb_bump_pc() + ",Y"          }
-    fn absolute(&mut self) -> ~str           { self.disw_bump_pc()                 }
-    fn absolute_x(&mut self) -> ~str         { self.disw_bump_pc() + ",X"          }
-    fn absolute_y(&mut self) -> ~str         { self.disw_bump_pc() + ",Y"          }
-    fn indexed_indirect_x(&mut self) -> ~str { ~"(" + self.disb_bump_pc() + ",X)"  }
-    fn indirect_indexed_y(&mut self) -> ~str { ~"(" + self.disb_bump_pc() + "),Y"  }
+    fn immediate(&mut self) -> ~str          { format!("{}{}", "#", self.disb_bump_pc()) }
+    fn accumulator(&mut self) -> ~str        { "".to_str()                               }
+    fn zero_page(&mut self) -> ~str          { self.disb_bump_pc()                       }
+    fn zero_page_x(&mut self) -> ~str        { self.disb_bump_pc() + ",X"                }
+    fn zero_page_y(&mut self) -> ~str        { self.disb_bump_pc() + ",Y"                }
+    fn absolute(&mut self) -> ~str           { self.disw_bump_pc()                       }
+    fn absolute_x(&mut self) -> ~str         { self.disw_bump_pc() + ",X"                }
+    fn absolute_y(&mut self) -> ~str         { self.disw_bump_pc() + ",Y"                }
+    fn indexed_indirect_x(&mut self) -> ~str { format!("({},X)", self.disb_bump_pc())    }
+    fn indirect_indexed_y(&mut self) -> ~str { format!("({}),Y", self.disb_bump_pc())    }
 
     // The main disassembly routine.
     #[inline(never)]
